b5b46d16f03d6192ba834f034d9010b05195576d
anas <anas.abbal66@gmail.com>
2024-01-04 02:12:22 +0100

    Initial commit

---
+++ user-service/src/test/java/com/nas/deliv/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nas.deliv.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
