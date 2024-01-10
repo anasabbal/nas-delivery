c1a2753b2339d13cfdb9fb04e65884fd4eea0af5
anas <anas.abbal66@gmail.com>
2024-01-10 19:10:01 +0100

    add some unit test for CustomerServiceTest

--- user-service/src/test/java/com/nas/deliv/userservice/service/CustomerServiceTest.java
+++ user-service/src/test/java/com/nas/deliv/userservice/service/CustomerServiceTest.java
@@ -9,18 +9,24 @@ import com.nas.deliv.userservice.repository.ConfirmationTokenRepository;
 import com.nas.deliv.userservice.repository.CustomerRepository;
 import com.nas.deliv.userservice.service.customer.CustomerServiceImpl;
 import exception.BusinessException;
+import exception.ExceptionPayloadFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.SimpleMailMessage;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-class CustomerServiceImplTest {
+class CustomerServiceTest {
 
     @Mock
     private CustomerRepository customerRepository;
@@ -41,11 +47,9 @@ class CustomerServiceImplTest {
 
     @Test
     void testCreateNewCustomer() {
-        // Arrange
-        // Arrange
         CustomerCreatedCommand command = new CustomerCreatedCommand();
-        command.setFirstName("John");
-        command.setLastName("Doe");
+        command.setFirstName("Anas");
+        command.setLastName("Abbal");
         command.setPhoneNumber("123456789");
         command.setEmail("john@example.com");
         command.setPassword("password123");
@@ -57,32 +61,73 @@ class CustomerServiceImplTest {
         ConfirmationToken confirmationToken = new ConfirmationToken(customer);
         when(confirmationTokenRepository.save(any(ConfirmationToken.class))).thenReturn(confirmationToken);
 
-        // Act
         String result = customerService.create(command);
 
-        // Assert
         verify(customerRepository, times(1)).existsCustomerByAccountInformation_Email(command.getEmail());
         verify(customerRepository, times(1)).save(any(Customer.class));
         verify(confirmationTokenRepository, times(1)).save(any(ConfirmationToken.class));
         verify(emailService, times(1)).sendEmail(any(SimpleMailMessage.class));
-
-        // Add more assertions based on your specific requirements
-        // Example: Check the result or additional verifications
     }
-
     @Test
     void testCreateCustomerWithExistingEmail() {
-        // Arrange
+        CustomerCreatedCommand command = createTestCommand();
+
+        when(customerRepository.existsCustomerByAccountInformation_Email(anyString())).thenReturn(true);
+
+        try {
+            customerService.create(command);
+        } catch (BusinessException e) {
+            throw new BusinessException(ExceptionPayloadFactory.EMAIL_ALREADY_EXIST.get());
+        }
+
+        // Verify that the save methods and sendEmail method were not called in this case.
+        verify(customerRepository, never()).save(any());
+        verify(confirmationTokenRepository, never()).save(any());
+        verify(emailService, never()).sendEmail(any());
+    }
+    @Test
+    void testGetCustomers() {
+        // Create a test Pageable
+        Pageable pageable = createTestPageable();
+
+        // Create a test Page<Customer>
+        Page<Customer> testPage = createTestPage();
+
+        // Mock the repository call
+        when(customerRepository.findAllByDeletedFalse(any(Pageable.class))).thenReturn(testPage);
+
+        // Call the service method
+        Page<Customer> resultPage = customerService.getCustomers(pageable);
+
+        // Add assertions based on your business logic
+        assertNotNull(resultPage);
+        assertEquals(testPage.getContent().size(), resultPage.getContent().size());
+
+        // Verify repository calls
+        verify(customerRepository, times(1)).findAllByDeletedFalse(any(Pageable.class));
+    }
+    private CustomerCreatedCommand createTestCommand() {
         CustomerCreatedCommand command = new CustomerCreatedCommand();
-        command.setFirstName("John");
-        command.setLastName("Doe");
+        command.setFirstName("Anas");
+        command.setLastName("Abbal");
         command.setPhoneNumber("123456789");
         command.setEmail("john@example.com");
         command.setPassword("password123");
         command.setClientType(ClientType.PERSON);
 
-        when(customerRepository.existsCustomerByAccountInformation_Email(command.getEmail())).thenReturn(true);
+        return command;
+    }
 
-        assertThrows(BusinessException.class, () -> customerService.create(command));
+    private Customer createTestCustomer() {
+        CustomerCreatedCommand command = createTestCommand();
+        return Customer.create(command);
+    }
+
+    private Pageable createTestPageable() {
+        return Pageable.unpaged();
+    }
+
+    private Page<Customer> createTestPage() {
+        return new PageImpl<>(Collections.singletonList(createTestCustomer()));
     }
 }
