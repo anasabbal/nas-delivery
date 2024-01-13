c973fdd3f7e6ab0bd46b7bc54eedf4c917b0744b
anas <anas.abbal66@gmail.com>
2024-01-13 19:18:05 +0100

    add FeignClient and implement caching

--- user-service/src/main/java/com/nas/deliv/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/nas/deliv/userservice/UserServiceApplication.java
@@ -13,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class UserServiceApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
